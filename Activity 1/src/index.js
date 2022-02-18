adaf60c6b6a271f8310502beb56eefe276f8ea92
anemus29 <sumenaalvymae@gmail.com>
2022-02-18 21:50:59 +0800

    first commit

---
+++ Activity 1/src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hi, Alvy Mae R. Sumena! Welcome to Code Sandbox :)</h1>
+<div>
+  She is currently a 3rd year BSIT student studying at La Salle University Ozamiz City. You can follow her on her instagram account by clicking the button below. 
+  <a href="https://www.instagram.com/alvy.rs/" target="_blank" rel="noopener noreferrer">Instagram Account </a>
+  <img src="https://wallpapers.com/images/high/black-and-white-aesthetic-glowing-keyboard-45cx71l747whidc4.jpg" alt="black and white keyboard" >
+</div>
+`;
